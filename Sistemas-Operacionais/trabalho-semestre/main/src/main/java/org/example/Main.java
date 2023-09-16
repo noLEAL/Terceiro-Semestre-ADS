2274fba011423b62ffd6b4d03bd991d17d872f5e
noLEAL <83329725+noLEAL@users.noreply.github.com>
2023-09-16 14:55:46 -0300

    .

---
+++ Sistemas-Operacionais/trabalho-semestre/main/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int opition;
+
+        Scanner key = new Scanner(System.in);
+
+        opition = key.nextInt();
+
+        switch (opition) {
+            case 1 -> {}
+            case 2 -> {}
+            case 3 -> {}
+            case 4 -> {}
+            case 5 -> {}
+            case 6 -> {}
+
+        }
+    }
+}
